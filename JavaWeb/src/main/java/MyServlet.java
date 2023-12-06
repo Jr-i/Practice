1ff4773ab1515c7c754c203170000d7153cd2a75
Jr-Desktop <Jr@outlook.com>
2023-12-06 19:19:07 +0800

    修正重写的service方法

--- JavaWeb/src/main/java/MyServlet.java
+++ JavaWeb/src/main/java/MyServlet.java
@@ -1,20 +1,20 @@
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet("/bill")
 public class MyServlet extends HttpServlet {
     @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+    protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         req.setCharacterEncoding("utf-8");
         String name = req.getParameter("name");
 
         String info = "yes";
-        if ("菠萝".equals(name)){
+        if ("菠萝".equals(name)) {
             info = "菠萝不行！";
         }
 
